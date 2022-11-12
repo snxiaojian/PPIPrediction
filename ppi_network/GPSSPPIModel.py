fix model forward , add pssm normalization

ppi_network/GPSSPPIModel.py
@@ -6,12 +6,15 @@ import random
 
 class GPSSPPIModel(torch.nn.Module):
 
-    def __init__(self):
+    def __init__(self, batch_size, device, pick_num):
         super(GPSSPPIModel,self).__init__()
         torch.backends.cudnn.enabled = False
-        self.pick_num = 50
+        self.batch_size = batch_size
+        self.device = device
+        self.pick_num = pick_num
         self.g_embedding_size = 256
         self.drop = 0.2
+
         # final residue size before combine acid
         self.residue_out_dim = 64
         # mix graph and pssm feature
@@ -60,29 +63,43 @@ class GPSSPPIModel(torch.nn.Module):
         return output
     
     def forward_part(self, G_residue, go_embedding, indexes, pssm):
-        device = indexes.device
-        batch_size = indexes.shape[0]
+        # indexes' shape is (batch_size, pick_num + 1)
         split = torch.split(indexes, [self.pick_num,1], dim=1)
+        # split indexes to real indexes and sequence length of protein
         indexes = split[0]
+        # get the residue feature of protein
+        total_residue_in_graph = G_residue.ndata['feat'].shape[0]
+        total_residue_in_graph = torch.ones(self.batch_size * self.pick_num) * total_residue_in_graph
+        total_residue_in_graph = total_residue_in_graph.type(torch.IntTensor).to(self.device)
+        # the protein length of a batch of protein , shape is (batch_size)
         protein_length = torch.squeeze(split[1])
-        indexes_in_batch =  torch.cumsum(protein_length, dim=0)
-        indexes_in_batch = torch.roll(indexes_in_batch, shifts=1, dims=0)
-        indexes_in_batch[0] = 0
-        indexes_in_batch = indexes_in_batch.unsqueeze(1)
-        indexes_in_batch = torch.add(indexes, indexes_in_batch).reshape(-1).type(torch.IntTensor).to(device)
+        indexes_cumsum =  torch.cumsum(protein_length, dim=0)
+        indexes_cumsum = torch.roll(indexes_cumsum, shifts=1, dims=0)
+        indexes_cumsum[0] = 0
+        indexes_cumsum = indexes_cumsum.unsqueeze(1)
+        # indexes is from 0 to pick_num, so we need to add indexes_in_batch to get the real index in batch
+        indexes_in_batch = torch.add(indexes, indexes_cumsum).reshape(-1).to(self.device)
         
+        indexes_in_batch_replace = torch.where(indexes_in_batch < 0, total_residue_in_graph, indexes_in_batch)
         g_feature = self.relu(self.gcn1(G_residue,G_residue.ndata['feat']))
         g_feature = g_feature.reshape(-1,self.g_embedding_size)
         g_feature = self.relu(self.gcn2(G_residue, g_feature))
         g_feature = g_feature.reshape(-1,self.g_embedding_size)
         g_feature = self.relu(self.gcn3(G_residue, g_feature))
         g_feature = g_feature.reshape(-1,self.g_embedding_size)
-        selected_feature = torch.index_select(g_feature, 0, indexes_in_batch)
+        # add zero to last row
+        zero = torch.zeros((1, self.g_embedding_size)).type(torch.FloatTensor).to(self.device)
+        g_feature = torch.cat((g_feature, zero), dim = 0)
+        
+        # if the protein length is less than pick_num, we need to add zero to the residue feature
+        # so that the residue feature can be the same shape as indexes
+        # use last row of zero to replace the missing residue feature
+        selected_feature = torch.index_select(g_feature, 0, indexes_in_batch_replace)
         
         residue_feature = torch.cat((selected_feature, pssm.reshape(-1, self.pssm_size)), dim=1)
         
         g_feature = self.relu(self.fc_g1(residue_feature))
-        g_feature = g_feature.reshape(batch_size, -1)
+        g_feature = g_feature.reshape(self.batch_size, -1)
         g_feature = self.relu(self.fc_g2(g_feature))
         g_feature = self.relu(self.fc_g3(g_feature))
 

ppi_network/ProteinFeatureLoader.py
@@ -12,14 +12,15 @@ from data_process.util import records_from_filtered_input, gene_from_record
 from data_process.pssm.PSSMGenerator import PSSMGenerator
 from ppi_network.ResiduePicker import ResiduePicker
 
-device = torch.device('cuda')
+
 class ProteinFeatureLoader:
-    def __init__(self):
+    def __init__(self, pick_num, device):
+        self.device = device
         self.anotations = GOAParser.parsed_annotation()
         self.records, self.species_dict = records_from_filtered_input()
         self.residue_feature_loader = ResidueFeatureLoader()
         self.all_embedding_dict = self.load_embedding_dict()
-        self.pick_number = 50
+        self.pick_number = pick_num
     
     def load_embedding_dict(self):
         embedding_dict = numpy.load("./data/go/go_embeddings.npy", allow_pickle=True)
@@ -56,9 +57,9 @@ class ProteinFeatureLoader:
         
         # residue graph
         residue_feature = self.residue_feature_loader.load_residue_feature(record)
-        residue_feature = torch.Tensor(residue_feature).to(device)
+        residue_feature = torch.Tensor(residue_feature).to(self.device)
         adj_residue = spp.coo_matrix(contact_map)
-        G_residue = dgl.from_scipy(adj_residue, device=device)
+        G_residue = dgl.from_scipy(adj_residue, device=self.device)
         G_residue.ndata['feat'] = residue_feature
         # go embedding
         go_embedding = []
@@ -66,13 +67,13 @@ class ProteinFeatureLoader:
             if go_id in self.all_embedding_dict:
                 e = self.all_embedding_dict[go_id].tolist()
                 go_embedding.append(e)
-        go_embedding = torch.Tensor(go_embedding).to(device)
+        go_embedding = torch.Tensor(go_embedding).to(self.device)
         go_embedding = torch.sum(go_embedding, dim=0)
         # pssm feature
         pssm = PSSMGenerator.readFromPSSM(pssm_file)
         
         
-        # pick 50 residues
+        # pick  residues
         indexes = ResiduePicker(record=record, pssm=pssm).pick_residue(number=self.pick_number)
         # last index add length of protein
         indexes.append(lenth_of_protein)
@@ -85,10 +86,12 @@ class ProteinFeatureLoader:
                 picked_pssm.append(pssm[index])
             else:
                 picked_pssm.append([0] * 20)
-        picked_pssm = torch.Tensor(picked_pssm).to(device)
-        indexes = torch.Tensor(indexes).to(device)
+        picked_pssm = torch.Tensor(picked_pssm).to(self.device)
+        norm_picked_pssm = torch.nn.functional.normalize(picked_pssm, dim=1)
+        indexes = torch.IntTensor(indexes).to(self.device)
         
-        return G_residue, go_embedding, picked_pssm, indexes
+        return G_residue, go_embedding, norm_picked_pssm, indexes
 
 if __name__ == "__main__":
-    ProteinFeatureLoader().default_loader("P47756")
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    ProteinFeatureLoader(pick_num= 50 ,device=device).default_loader("A9YTQ3")

train.py
@@ -11,7 +11,7 @@ sys.path.append("./")
 from ppi_network.PPIDataset import PPIDataset, collate
 from ppi_network.GPSSPPIModel import GPSSPPIModel
 
-device = torch.device('cuda')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def predicting(model, loader):
     model.eval()
@@ -27,31 +27,34 @@ def predicting(model, loader):
             total_labels = torch.cat((total_labels.cpu(), y.float().cpu()), 0)
             
     return total_labels.numpy().flatten(),total_preds.numpy().flatten()
-
+    
 def toTensor(list_of_tensors):
     tensor_of_tensors = torch.stack((list_of_tensors))
     return tensor_of_tensors.to(device)
-    
-def train():
 
+def train():
     train_losses = []
     train_accs = []
     
-    train_dataset = PPIDataset(type='train')
+    shuffle = False
+    batch_size = 100
+    pick_num = 50
+    
+    train_dataset = PPIDataset(type='train', pick_num=pick_num, device=device)
     train_loader = DataLoader(dataset=train_dataset,
-               batch_size=100,
-               shuffle=True,
+               batch_size=batch_size,
+               shuffle=shuffle,
                drop_last=False,
                collate_fn=collate)
     
-    test_dataset = PPIDataset(type='test')
+    test_dataset = PPIDataset(type='test', pick_num=pick_num, device=device)
     test_loader = DataLoader(dataset=test_dataset,
-               batch_size=100,
-               shuffle=True,
+               batch_size=batch_size,
+               shuffle=shuffle,
                drop_last=False,
                collate_fn=collate)
     
-    model = GPSSPPIModel().to(device)
+    model = GPSSPPIModel(batch_size=batch_size, device=device, pick_num=pick_num).to(device=device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     criterion = torch.nn.BCELoss()
     